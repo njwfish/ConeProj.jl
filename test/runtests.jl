57770afb708b25bfa507af6cd8312bbe1ae41366
njwfish <njwfish@gmail.com>
2023-07-05 15:08:32 -0700

    refactor

--- test/runtests.jl
+++ test/runtests.jl
@@ -17,14 +17,14 @@ end
     A, b, C, d = sim(rng);
 
     # nn constrained p = 0
-    coefs, passive_set, R, status = coneB(A, b);
+    coefs, passive_set, R, status = nnls(A, b);
     @test status == 1
     _, r = qr(A[:, passive_set])
     _coefs = solvex(r, A[:, passive_set], b)
     @test all(A' * (b - A[:, passive_set] * _coefs) .<= 1e-8)
 
     # eq and nn constrained p = 0
-    coefs, passive_set, R, status = coneBEq(A, b, C, d);
+    coefs, passive_set, R, status = ecnnls(A, b, C, d);
     @test status == 1
     _, r = qr(A[:, passive_set])
     _coefs, lambd = solvexeq(r, A[:, passive_set], b, C[:, passive_set], d)
